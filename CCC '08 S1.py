7e2235a389babc104367966d84d7f4ecf373aeeb
jasonl0209 <48419911+jasonl0209@users.noreply.github.com>
2020-06-26 21:54:24 -0400

    Delete CCC '08 S1.py

--- CCC '08 S1.py
+++
@@ -1,9 +0,0 @@
-lst = []
-inp_getter = [1, 2]
-lst_temp = []
-while inp_getter[0] != "Waterloo":
-    inp_getter = input().split(" ")
-    lst.append(inp_getter[0])
-    lst.append(int(inp_getter[1]))
-    lst_temp.append(int(inp_getter[1]))
-print(lst[lst.index(min(lst_temp)) - 1])
